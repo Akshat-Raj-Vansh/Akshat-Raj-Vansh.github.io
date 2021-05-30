567d2d537ebe48dd1e88feed61c2cbe020559402
AkshatRajVansh <akshat03rajvansh@gmail.com>
2021-05-30 21:33:50 +0530

    removed constants

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,11 +1,17 @@
-class Constants{
-  static const BLOG_API = 'https://adityadroid-medium.herokuapp.com/@adityadroid';
+class Constants {
+  static const BLOG_API =
+      'https://adityadroid-medium.herokuapp.com/@adityadroid';
   static const MEDIUM_IMAGE_CDN = 'https://miro.medium.com/fit/c/700/300/';
-  static const BLOG_URL = 'http://www.medium.com/@adityadroid/';
-  static const PROFILE_MEDIUM = 'http://www.medium.com/@adityadroid';
-  static const PROFILE_LINKEDIN = 'https://www.linkedin.com/in/adityagurjar/';
-  static const PROFILE_FACEBOOK = 'https://www.facebook.com/adityaadi333';
-  static const PROFILE_GITHUB = 'https://github.com/adityadroid/';
-  static const PROFILE_TWITTER = 'https://twitter.com/adityadroid';
-  static const PROFILE_INSTAGRAM = 'https://www.instagram.com/adityadroid/';
+  static const BLOG_URL = 'https://www.linkedin.com/in/akshat-r-0589b4b1/';
+  static const PROFILE_MEDIUM =
+      'https://www.linkedin.com/in/akshat-r-0589b4b1/';
+  static const PROFILE_LINKEDIN =
+      'https://www.linkedin.com/in/akshat-r-0589b4b1/';
+  static const PROFILE_FACEBOOK =
+      'https://www.linkedin.com/in/akshat-r-0589b4b1/';
+  static const PROFILE_GITHUB = 'https://github.com/Akshat-Raj-Vansh/';
+  static const PROFILE_TWITTER =
+      'https://www.linkedin.com/in/akshat-r-0589b4b1/';
+  static const PROFILE_INSTAGRAM =
+      'https://www.linkedin.com/in/akshat-r-0589b4b1/';
 }
